ce4a7d4462f227bc517a19883201054809a6e363
atharva434 <85440501+atharva434@users.noreply.github.com>
2021-11-24 23:47:24 +0530

    ok

--- predict/views.py
+++ predict/views.py
@@ -9,16 +9,9 @@ from keras.applications.imagenet_utils import decode_predictions
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 # Create your views here.
 model_graph = Graph()
-valid_datagen = ImageDataGenerator(rescale=1./255)
-valid_set = valid_datagen.flow_from_directory('models/valid',
-                                            target_size=(224, 224),
-                                            batch_size=50,
-                                            class_mode='categorical')
+
 model=load_model('models\plant_disease_detection_MobileNet.h5')
-def get_class_string_from_index(index):
-   for class_string, class_index in valid_set.class_indices.items():
-      if class_index == index:
-         return class_string
+
 # path='models/'
 # filename = 'densenet.pkl'
 # model = pickle.load(open(path+filename, 'rb'))
@@ -49,9 +42,9 @@ def predictImage(request):
     classes_x=np.argmax(predi)
     
     print(classes_x)
-    # classes=['Grape__Leaf_blight(Isariopsis_Leaf_Spot)', 'Corn_(maize)__Northern_Leaf_Blight', 'Soybean_healthy', 'Apple_Cedar_apple_rust', 'Tomato_Leaf_Mold', 'Tomato_Late_blight', 'Cherry(including_sour)__Powdery_mildew', 'Grape_Esca(Black_Measles)', 'Squash__Powdery_mildew', 'Apple_healthy', 'Tomato_Early_blight', 'Orange_Haunglongbing(Citrus_greening)', 'Corn_(maize)__healthy', 'Potato_healthy', 'Peach_Bacterial_spot', 'Tomato_Target_Spot', 'Tomato_Bacterial_spot', 'Potato_Late_blight', 'Blueberry_healthy', 'Pepper,_bell_Bacterial_spot', 'Apple_Black_rot', 'Pepper,_bell_healthy', 'Apple_Apple_scab', 'Peach_healthy', 'Strawberry_healthy', 'Tomato_Septoria_leaf_spot', 'Tomato_Tomato_mosaic_virus', 'Potato_Early_blight', 'Cherry(including_sour)__healthy', 'Tomato_Spider_mites Two-spotted_spider_mite', 'Strawberry_Leaf_scorch', 'Tomato_healthy', 'Tomato_Tomato_Yellow_Leaf_Curl_Virus', 'Corn(maize)__Cercospora_leaf_spot Gray_leaf_spot', 'Grape_healthy', 'Raspberry_healthy', 'Corn(maize)__Common_rust', 'Grape___Black_rot']
-    # MaxPosition=np.argmax(predi)  
-    prediction_label=get_class_string_from_index(classes_x)
+    classes=['Apple__Apple_scab', 'Apple_Black_rot', 'Apple_Cedar_apple_rust', 'Apple_healthy',  'Blueberry_healthy', 'Cherry(including_sour)___Powdery_mildew','Cherry_(including_sour)__healthy','Corn(maize)__Cercospora_leaf_spot Gray_leaf_spot', 'Corn(maize)__Common_rust', 'Corn_(maize)___Northern_Leaf_Blight','Corn_(maize)__healthy',  'Grape_Black_rot', 'Grape_Esca(Black_Measles)', 'Grape__Leaf_blight(Isariopsis_Leaf_Spot)', 'Grape__healthy', 'Orange_Haunglongbing(Citrus_greening)',  'Peach__Bacterial_spot', 'Peach_healthy', 'Pepper,_bell_Bacterial_spot', 'Pepper,_bell_healthy', 'Potato_Early_blight' , 'Potato_Late_blight', 'Potato_healthy', 'Raspberry_healthy', 'Soybean_healthy', 'Squash_Powdery_mildew', 'Strawberry_Leaf_scorch', 'Strawberry_healthy', 'Tomato_Bacterial_spot',  'Tomato_Early_blight', 'Tomato_Late_blight', 'Tomato_Leaf_Mold', 'Tomato_Septoria_leaf_spot', 'Tomato__Spider_mites Two-spotted_spider_mite','Tomato__Target_Spot', 'Tomato_Tomato_Yellow_Leaf_Curl_Virus', 'Tomato_Tomato_mosaic_virus', 'Tomato__healthy']
+    MaxPosition=np.argmax(predi)  
+    prediction_label=prediction_label=classes[MaxPosition]
     print(prediction_label)
 
     context={'filePathName':filePathName,'predictedLabel':prediction_label}
