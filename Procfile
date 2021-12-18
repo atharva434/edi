43b178d8ad483658af11191f928a7577845ca9ab
atharva434 <85440501+atharva434@users.noreply.github.com>
2021-12-18 14:10:21 +0530

    um

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn plantdiseaserecoginition.wsgi --log-file -
--- plantdiseaserecognition/urls.py
+++ plantdiseaserecognition/urls.py
@@ -28,3 +28,5 @@ urlpatterns = [
     url('predictImage',views.predictImage,name='predictImage'),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    # url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    # url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
