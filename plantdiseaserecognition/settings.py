6e470a4cfe9d356bf5555154b08213d572fc5820
atharva434 <85440501+atharva434@users.noreply.github.com>
2021-12-18 14:13:04 +0530

    a

--- plantdiseaserecognition/settings.py
+++ plantdiseaserecognition/settings.py
@@ -127,8 +127,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS=[
-    os.path.join(BASE_DIR,'static')
+    os.path.join(BASE_DIR,'atharva','static')
 ]
 
 # Default primary key field type
